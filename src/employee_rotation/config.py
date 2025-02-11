756a96a1cf295ab44fe6e1ddda1bca1553737f68
Saoud Salah Eddine <salah_fh@outlook.com>
2025-02-10 21:56:17 -0600

    Update config rotation.

--- src/employee_rotation/config.py
+++ src/employee_rotation/config.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass
 class Config:
     INPUT_FOLDER = Path().home() / "employee_rotation"
     OUTPUT_FOLDER = Path().home() / "employee_rotation"
-    rotations = 20
+    rotations = 14
     rotation_length_in_months = 6.1
 
     def __post_init__(self):
